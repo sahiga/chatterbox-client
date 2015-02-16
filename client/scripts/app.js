26eddf9ea14b57bff0b2f81c493161ab43fd7d21
sahiga <sahiga@gmail.com>
2015-02-16 15:17:23 -0800

    Get and add messages to chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,18 @@
-// YOUR CODE HERE:
+var displayMessages = function(response) {
+  var messages = response.results;
+  var $messageList = $('#message-list');
+
+  for (var i = 0; i < messages.length; i++) {
+    var message = messages[i];
+    var messageDiv =
+      '<div class="message"><span class="username">' +
+        message.username + '</span>' + message.text +
+      '</div>';
+    $messageList.append(messageDiv);
+  }
+};
+
+var getMessages = function() {
+  $.get('https://api.parse.com/1/classes/chatterbox', displayMessages);
+};
 
