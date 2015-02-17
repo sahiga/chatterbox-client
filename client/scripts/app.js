c039aeb7cdfc60f766a5836e2410a59cc381eba6
sahiga <sahiga@gmail.com>
2015-02-16 17:31:32 -0800

    Convert to app namespace

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,51 @@
-// var messageCount = 0;
-var lastCreated = '2015-02-17T00:33:32.494Z';
+var app = {
+  lastCreated: '2015-02-17T00:33:32.494Z',
 
-var cleanData = function(unsafe) {
-  unsafe = unsafe || 'NA';
-  return he.encode(unsafe);
-};
+  init: function() {
+    var context = this;
+    setInterval(function() {
+      context.getNewMessages();
+    }, 5000);
+  },
 
-var displayMessages = function(messages) {
-  var $messageList = $('#message-list');
+  cleanData: function(unsafe) {
+    unsafe = unsafe || 'NA';
+    return he.encode(unsafe);
+  },
 
-  for (var i = 0; i < messages.length; i++) {
-    var message = messages[i];
-    var username = cleanData(message.username);
-    var text = cleanData(message.text);
-    var messageDiv =
-      '<div class="message"><span class="username">' +
-        username + '</span>' + text + '</div>';
-    $messageList.prepend(messageDiv);
+  displayMessages: function(messages) {
+    var $messageList = $('#message-list');
+
+    for (var i = 0; i < messages.length; i++) {
+      var message = messages[i];
+      var username = this.cleanData(message.username);
+      var text = this.cleanData(message.text);
+      var messageDiv =
+        '<div class="message"><span class="username">' +
+          username + '</span>' + text + '</div>';
+      $messageList.prepend(messageDiv);
+    }
+  },
+
+  getNewMessages: function() {
+    var context = this;
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      data: 'order=-createdAt&where={"createdAt":{"$gte":"' + context.lastCreated + '"}}',
+      success: function(response) {
+        var messages = response.results;
+        context.lastCreated = messages[0].createdAt; // get most up-to-date timestamp
+        context.displayMessages(messages);
+      },
+      error: function() {
+        console.log('chatterbox: Failed to get message');
+      }
+    });
   }
 };
 
-var getNewMessages = function() {
-  $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    data: 'order=-createdAt&where={"createdAt":{"$gte":"' + lastCreated + '"}}',
-    success: function(response) {
-      var messages = response.results;
-      lastCreated = messages[0].createdAt;
-      displayMessages(messages);
-    },
-    error: function() {
-      console.log('chatterbox: Failed to get message');
-    }
-  });
-};
+app.init();
 
-setInterval(getNewMessages, 5000);
+
 
