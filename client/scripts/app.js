fcc1ca4e0cdda6b5b89c5c2856f61351e90102b5
sahiga <sahiga@gmail.com>
2015-02-16 17:11:06 -0800

    Update message stream

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,12 @@
+// var messageCount = 0;
+var lastCreated = '2015-02-17T00:33:32.494Z';
+
 var cleanData = function(unsafe) {
   unsafe = unsafe || 'NA';
   return he.encode(unsafe);
 };
 
-var displayMessages = function(response) {
-  var messages = response.results;
+var displayMessages = function(messages) {
   var $messageList = $('#message-list');
 
   for (var i = 0; i < messages.length; i++) {
@@ -13,14 +15,26 @@ var displayMessages = function(response) {
     var text = cleanData(message.text);
     var messageDiv =
       '<div class="message"><span class="username">' +
-        username + '</span>' + text +
-      '</div>';
-    $messageList.append(messageDiv);
+        username + '</span>' + text + '</div>';
+    $messageList.prepend(messageDiv);
   }
 };
 
-var getMessages = function() {
-  $.get('https://api.parse.com/1/classes/chatterbox', displayMessages);
+var getNewMessages = function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: 'order=-createdAt&where={"createdAt":{"$gte":"' + lastCreated + '"}}',
+    success: function(response) {
+      var messages = response.results;
+      lastCreated = messages[0].createdAt;
+      displayMessages(messages);
+    },
+    error: function() {
+      console.log('chatterbox: Failed to get message');
+    }
+  });
 };
 
-getMessages();
+setInterval(getNewMessages, 5000);
+
