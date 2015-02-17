043eabc1911a84000637c2abbfbcedee5a8781e3
Hack Reactor Students <accounts@hackreactor.com>
2015-02-16 19:28:19 -0800

    Allow user to send messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,51 +1,78 @@
-var app = {
-  lastCreated: '2015-02-17T00:33:32.494Z',
+$(document).ready(function() {
+  var app = {
+    lastCreated: '2015-02-17T00:33:32.494Z',
 
-  init: function() {
-    var context = this;
-    setInterval(function() {
-      context.getNewMessages();
-    }, 5000);
-  },
+    init: function() {
+      var context = this;
+      setInterval(function() {
+        context.fetch();
+      }, 5000);
 
-  cleanData: function(unsafe) {
-    unsafe = unsafe || 'NA';
-    return he.encode(unsafe);
-  },
+      $('#send-message').click(this.send);
+    },
 
-  displayMessages: function(messages) {
-    var $messageList = $('#message-list');
+    cleanData: function(unsafe) {
+      unsafe = unsafe || 'NA';
+      return he.encode(unsafe);
+    },
 
-    for (var i = 0; i < messages.length; i++) {
-      var message = messages[i];
-      var username = this.cleanData(message.username);
-      var text = this.cleanData(message.text);
-      var messageDiv =
-        '<div class="message"><span class="username">' +
-          username + '</span>' + text + '</div>';
-      $messageList.prepend(messageDiv);
-    }
-  },
+    displayMessages: function(messages) {
+      var $messageList = $('#chats');
 
-  getNewMessages: function() {
-    var context = this;
-    $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      data: 'order=-createdAt&where={"createdAt":{"$gte":"' + context.lastCreated + '"}}',
-      success: function(response) {
-        var messages = response.results;
-        context.lastCreated = messages[0].createdAt; // get most up-to-date timestamp
-        context.displayMessages(messages);
-      },
-      error: function() {
-        console.log('chatterbox: Failed to get message');
+      for (var i = 0; i < messages.length; i++) {
+        var message = messages[i];
+        var username = this.cleanData(message.username);
+        var text = this.cleanData(message.text);
+        var messageDiv =
+          '<div class="message"><span class="username">' +
+            username + '</span>' + text + '</div>';
+        $messageList.prepend(messageDiv);
       }
-    });
-  }
-};
+    },
 
-app.init();
+    fetch: function() {
+      var context = this;
+      $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'GET',
+        data: 'order=-createdAt&where={"createdAt":{"$gte":"' + context.lastCreated + '"}}',
+        success: function(response) {
+          var messages = response.results;
+          context.lastCreated = messages[0].createdAt; // get most up-to-date timestamp
+          context.displayMessages(messages);
+        },
+        error: function() {
+          console.log('chatterbox: Failed to get message');
+        }
+      });
+    },
 
+    send: function() {
+      console.log("Sent!");
+      var message = {
+        username: $('#user').val(),
+        text: $('#message').val(),
+        roomname: $('#room').val()
+      };
+
+      $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox',
+        type: 'POST',
+        data: JSON.stringify(message),
+        contentType: 'application/json',
+        success: function() {
+          $('#status').text('Your message was successfully sent!')
+                      .addClass('success');
+        },
+        error: function() {
+          $('#status').text('Your message failed :(')
+                      .addClass('failure');
+        }
+      });
+    }
+  };
+
+  app.init();
+});
 
 
