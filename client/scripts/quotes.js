d78de38a175321e3e725e0903c4274c6b0215957
Hack Reactor Students <accounts@hackreactor.com>
2015-02-16 21:35:20 -0800

    Connect to Chuck Norris API

---
+++ client/scripts/quotes.js
@@ -0,0 +1,28 @@
+var getQuote = function() {
+  // perform AJAX request to quotes api
+  $.ajax({
+    url: 'http://api.icndb.com/jokes/random',
+    type: 'GET',
+    success: function(response) {
+      var quote = response.value.joke;
+      var message = {
+        username : "Chuck Norris",
+        text: quote,
+        roomname: "Everywhere"
+      };
+      app.send(message);
+    },
+    error: function() {
+      console.log('quote not retrieved');
+    },
+    dataType: "jsonp"
+  });
+};
+
+setInterval(getQuote, 15000);
+
+
+
+
+
+
